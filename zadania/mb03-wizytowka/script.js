d61142f06166edcbccfeb7f0a36159db2a2da29e
unknown <user@V08D.edu2.zsl.poznan.pl>
2026-09-21 15:19:34 +0200

    MB3: Obsługa formularza w javascript

--- zadania/mb03-wizytowka/script.js
+++ zadania/mb03-wizytowka/script.js
@@ -7,3 +7,40 @@ for (const umiejetnosc of lista_umiejetnosci) {
     element.textContent = umiejetnosc;
     lista.appendChild(element);
 }
+
+const formularz = document.querySelector("#formularz-kontakt");
+const komunikat = document.querySelector("#komunikat");
+
+function pokaz_komunikat(tresc, rodzaj){
+    komunikat.textContent = tresc;
+    komunikat.classList.remove("blad", "sukces");
+    komunikat.classList.add(rodzaj);
+}
+
+formularz.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let imie = document.querySelector("#imie").value;
+    let email = document.querySelector("#email").value;
+    let temat = document.querySelector("#temat").value;
+    let tresc = document.querySelector("#tresc").value;
+
+    if (imie === ""){
+        pokaz_komunikat("Podaj imię.", "blad");
+        return ;
+    }
+    
+    if (email === ""){
+        pokaz_komunikat("Podaj email.", "blad");
+        return ;
+    }
+    
+    if (temat === ""){
+        pokaz_komunikat("Wybierz temat.", "blad");
+        return ;
+    }
+
+    pokaz_komunikat("Imię: " + imie + "\nTemat: " + temat, "sukces");
+
+    formularz.reset()
+})
