089833d7a1db5555fd85490fdcece32eb846b7f8
Eloszek1 <116819715+Eloszek1@users.noreply.github.com>
2026-09-25 18:48:28 +0200

    MB4: kopia projektu z MB3, włączenie modułów

---
+++ zadania/mb04-wizytowka-es6/script.js
@@ -0,0 +1,60 @@
+let lista_umiejetnosci = ["Matematyka", "C#", "JavaScript", "HTML", "CSS"];
+
+const lista = document.querySelector("#lista-umiejetnosci");
+
+for (const umiejetnosc of lista_umiejetnosci) {
+    let element = document.createElement("li");
+    element.textContent = umiejetnosc;
+    lista.appendChild(element);
+}
+
+const formularz = document.querySelector("#formularz-kontakt");
+const komunikat = document.querySelector("#komunikat");
+
+function pokaz_komunikat(tresc, rodzaj){
+    komunikat.textContent = tresc;
+    komunikat.classList.remove("blad", "sukces");
+    komunikat.classList.add(rodzaj);
+}
+
+formularz.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let imie = document.querySelector("#imie").value;
+    let email = document.querySelector("#email").value;
+    let temat = document.querySelector("#temat").value;
+    let tresc = document.querySelector("#tresc").value;
+
+    if (imie === ""){
+        pokaz_komunikat("Podaj imię.", "blad");
+        return ;
+    }
+    
+    if (email === ""){
+        pokaz_komunikat("Podaj email.", "blad");
+        return ;
+    }
+    
+    if (temat === ""){
+        pokaz_komunikat("Wybierz temat.", "blad");
+        return ;
+    }
+
+    pokaz_komunikat("Imię: " + imie + "\nTemat: " + temat, "sukces");
+
+    formularz.reset()
+})
+
+let ilosc_klikniec = 0;
+const ilosc_klikniec_blok = document.querySelector("#ilosc-klikniec");
+const klikacz = document.querySelector("#klikacz");
+
+klikacz.addEventListener("click", (event) => {
+    if (ilosc_klikniec == 9999){
+        ilosc_klikniec = 0;
+    }
+    ilosc_klikniec++;
+    ilosc_klikniec_blok.textContent = String(ilosc_klikniec).padStart(4, "0");
+})
+
+
